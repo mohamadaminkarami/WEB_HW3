556616a02cb93ddbc0d2b2219c5567db91704df8
Mohamadamin Karami <mohamadaminkarami.ce@gmail.com>
2022-01-19 14:34:14 +0330

    Feat: Add yarn createsuperuser

---
+++ server/src/utils/response-codes.js
@@ -0,0 +1,5 @@
+const RESPONSE_CODE = {
+  CONFLICT: 409,
+};
+
+export default RESPONSE_CODE;
--- server/src/utils/sequelize.js
+++ server/src/utils/sequelize.js
@@ -4,7 +4,16 @@ import config from "../config";
 const {
   DATABASE: { HOST, PASSWORD, PORT, USERNAME, DIALECT, NAME },
 } = config;
-const sequelize = new Sequelize({ host: HOST, password: PASSWORD, username: USERNAME, port: PORT, dialect: DIALECT, database: NAME, define: { freezeTableName: true } });
+const sequelize = new Sequelize({
+  host: HOST,
+  password: PASSWORD,
+  username: USERNAME,
+  port: PORT,
+  dialect: DIALECT,
+  database: NAME,
+  define: { freezeTableName: true },
+  logging: false,
+});
 
 try {
   await sequelize.authenticate();
