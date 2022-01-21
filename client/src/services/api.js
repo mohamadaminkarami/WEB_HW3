9bc1c75a2083403aa81e1a9d9a9fb6f0d04e3318
masihbr <masihbr@gmail.com>
2022-01-21 19:10:21 +0330

    Read backed url from .env

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BACKEND_URL = 'http://localhost:8080'
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 const API = axios.create({
     baseURL: BACKEND_URL,
